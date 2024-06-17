e944218d48b963478a6030f00a4b2db7b20715c1
mraduldubey03 <mradul97dubey@gmail.com>
2024-06-17 12:47:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='font-extrabold text-9xl text-red-800'
+    >Namaskar Mradul Dubey</h1>
+    </>
+  )
+}
+
+export default App
